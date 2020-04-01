5dad2a44c4f818ad105f885d316a003061229a9e
od2 <od2@localhost>
2020-04-02 02:46:35 +0700

    intial

---
+++ reddit_scraper/scraper.py
@@ -0,0 +1,50 @@
+#! usr/bin/python3
+import praw
+from praw.models import MoreComments
+import pandas as pd
+import datetime as dt
+import json
+import csv
+
+corpus = dict()
+output_file= 'conglomerate.csv'
+posts_limit= 1000
+
+with open('../config/config.json') as json_data_file:
+    data = json.load(json_data_file)['details']
+
+reddit = praw.Reddit(client_id=data['client_id'], \
+                     client_secret=data['client_secret'], \
+                     user_agent=data['user_agent'], \
+                     username=data['username'], \
+                     password=data['password'])
+
+topics_dict = { "title":[], \
+                "score":[], \
+                "id":[], "url":[], \
+                "comms_num": [], \
+                "created": [], \
+                "body":[]}
+
+data_dict = { "context":[], \
+                "text response":[]}
+
+subreddit_list = [reddit.subreddit('depression'), reddit.subreddit('anxiety'), reddit.subreddit('stress'), reddit.subreddit('CasualConversation'), reddit.subreddit('AnxietyDepression')]
+top_subreddit_list=[]
+for i in subreddit_list:
+    top_subreddit_list.append(i.top(limit=posts_limit))
+
+with open('../corpora/%s' % output_file, mode='w') as csv_file:
+    fieldnames=['context', 'text response']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    for top_subreddit in top_subreddit_list:
+        for submission in top_subreddit:
+            submission.comment_sort = 'best'
+            submission.comment_limit = 1
+            for top_level_comment in submission.comments:
+                if isinstance(top_level_comment, MoreComments):
+                    continue
+                writer.writerow({'context': submission.title, 'text response': top_level_comment.body})
+
+
